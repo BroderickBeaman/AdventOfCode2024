05191aeda7fcdd5622c0f0cd32db9d8ccb59525b
BroderickBeaman <broderick.beaman.dev@gmail.com>
2024-12-17 12:54:47 -0400

    Dec 17 part 2 solution (only works for my input as I reverse engineered the program)

--- src/dec17/Program.java
+++ src/dec17/Program.java
@@ -41,39 +41,6 @@ class Program {
         return toString();
     }
 
-    public Long runP2() {
-        long counter = registerA;
-        while (true) {
-            registerA = counter;
-            registerB = 0;
-            registerC = 0;
-            output = new ArrayList<>();
-            pointer = 0;
-
-            while (getOpCode() != null) {
-                int opCode = getOpCode();
-                int operand = getOperand();
-
-                switch (opCode) {
-                    case 0 -> adv(operand);
-                    case 1 -> bxl(operand);
-                    case 2 -> bst(operand);
-                    case 3 -> jnz(operand);
-                    case 4 -> bxc(operand);
-                    case 5 -> out(operand);
-                    case 6 -> bdv(operand);
-                    case 7 -> cdv(operand);
-                    default -> throw new RuntimeException("Unexpected opCode: " + opCode);
-                }
-            }
-
-            if (output.equals(program)) {
-                return counter;
-            }
-            counter++;
-        }
-    }
-
     private void adv(int operand) {
         registerA = registerA / (long) Math.pow(2, convertToComboOperand(operand));
         incrementPointer();
@@ -140,6 +107,44 @@ class Program {
             case 6 -> registerC;
             default -> throw new RuntimeException("Unexpected operand: " + literal);
         };
+    }
+
+    public Long runP2() {
+        return trySolution(0L, 0);
+    }
+
+    public Long trySolution(final Long regAInput, final Integer depth) {
+        registerA = regAInput;
+        registerB = 0;
+        registerC = 0;
+        pointer = 0;
+        output = new ArrayList<>();
+
+        runP1();
+
+        if (output.equals(program)) {
+            return regAInput;
+        }
+
+
+        for (long i = 0; i < 8; i++) {
+            long registerA = (regAInput << 3) + i;
+            long registerB = registerA % 8;
+            registerB = registerB ^ 2L;
+            long registerC = registerA >> registerB;
+            registerB = registerB ^ registerC;
+            registerB = registerB ^ 3;
+
+            if (registerB % 8L == program.get(program.size() - 1 - depth)) {
+                Long next = trySolution(registerA, depth + 1);
+                if (next == null) {
+                    continue;
+                }
+                return next;
+            }
+        }
+
+        return null;
     }
 
     @Override
