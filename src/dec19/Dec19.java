ae1512feb45fea834152c968928bff6413a8a185
Broderick Beaman <broderick.beaman@unb.ca>
2024-12-19 10:34:35 -0400

    Dec 19 Part 1

--- src/dec19/Dec19.java
+++ src/dec19/Dec19.java
@@ -2,9 +2,50 @@ package dec19;
 
 import framework.AOCParent;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Dec19 extends AOCParent {
+
+    Map<String, Boolean> designCache;
+    int maxPatternLength;
+
     @Override
     public void part1() {
+        final List<String> patterns = InputLoader.loadPatterns();
+        final List<String> designs = InputLoader.loadDesigns();
+        designCache = new HashMap<>();
+        maxPatternLength = patterns.stream().mapToInt(String::length).max().getAsInt();
+
+        long count = designs.stream().filter(design -> isPossible(patterns, design)).count();
+        printSolution(count);
+    }
+
+    private boolean isPossible(final List<String> patterns, String design) {
+
+        if (patterns.contains(design) || design.isEmpty()) {
+            return true;
+        }
+
+        if (designCache.containsKey(design)) {
+            return designCache.get(design);
+        }
+
+        for (int i = 1; i <= Math.min(maxPatternLength, design.length()); i++) {
+            String toTest = design.substring(0, i);
+            String nextDesign = design.substring(i);
+            if (patterns.contains(toTest)) {
+                boolean result = isPossible(patterns, nextDesign);
+                designCache.put(nextDesign, result);
+                if (result) {
+                    return true;
+                }
+            }
+        }
+
+        designCache.put(design, false);
+        return false;
 
     }
 
--- src/dec19/InputLoader.java
+++ src/dec19/InputLoader.java
@@ -2,5 +2,17 @@ package dec19;
 
 import framework.InputLoaderParent;
 
+import java.util.Arrays;
+import java.util.List;
+
 class InputLoader extends InputLoaderParent {
+
+    static List<String> loadPatterns() {
+        return Arrays.asList(loadLines().getFirst().split(", "));
+    }
+
+    static List<String> loadDesigns() {
+        List<String> lines = loadLines();
+        return lines.subList(2, lines.size());
+    }
 }
